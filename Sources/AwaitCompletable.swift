f4a3ee43c81907e27fb1af6596f30e7fc13a142c
JoongKwan Choi <joongkwan.choi@gmail.com>
2019-04-18 17:31:54 +0900

    Update AwaitCompletable.swift

--- Sources/AwaitCompletable.swift
+++ Sources/AwaitCompletable.swift
@@ -6,23 +6,24 @@
 //  Copyright © 2017년 tiny2n. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+public enum AwaitCompletableResult<T> {
+    case success(T)
+    case failure(Error)
+}
 
 public protocol AwaitCompletable {
     associatedtype AwaitCompletableType
     
     var queue: DispatchQueue { get }
-    var timeout: DispatchTimeInterval? { get }
+    var timeout: DispatchTimeInterval? { get set }
     
-    func execute(_ completion: @escaping (AwaitCompletableType) throws -> Void) throws
+    func execute(_ completion: @escaping (AwaitCompletableResult<AwaitCompletableType>) -> Void)
 }
 
 extension AwaitCompletable {
     public var queue: DispatchQueue {
-        return DispatchQueue(label: "com.tiny2n.queue.completable", attributes: .concurrent)
-    }
-    
-    public var timeout: DispatchTimeInterval? {
-        return nil
+        return DispatchQueue(label: "com.navercafe.queue.completable", attributes: .concurrent)
     }
 }
