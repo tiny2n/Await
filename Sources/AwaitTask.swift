bce249e0bc08d8dda935cf4ea1f65eea6e0f847b
joongkwan.choi <joongkwan.choi@navercorp.com>
2019-10-15 00:14:37 +0900

    add file AwaitTask

---
+++ Sources/AwaitTask.swift
@@ -0,0 +1,25 @@
+//
+//  AwaitTask.swift
+//  Await-Demo
+//
+//  Created by joongkwan.choi on 15/10/2019.
+//  Copyright © 2019 tiny2n. All rights reserved.
+//
+
+import Foundation
+
+final public class AwaitTask<Value> {
+    private var value: Value
+    
+    public init(value: Value) {
+        self.value = value
+    }
+    
+    public func await() -> Value {
+        return value
+    }
+}
+
+public func await<Value>(_ task: AwaitTask<Value>) throws -> Value {
+    return task.await()
+}
