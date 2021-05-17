8b27d34d813137188ee0da3f03172b7d09cb0d04
Joongkwan Choi <joongkwan.choi@navercorp.com>
2021-05-17 14:14:01 +0900

    Supported: build libraries for distribution YES

--- Await-Demo/ViewController.swift
+++ Await-Demo/ViewController.swift
@@ -7,51 +7,51 @@
 //
 import UIKit
 
-public class ACSum0To1000000000Odd: AwaitConcurrentSupport<Int> {
-    
-    public override func execute(_ completion: @escaping (AwaitCompletableResult<Int>) -> Void) {
-        var sum = 0
-        for i in 0...1000000000 where i % 2 == 1 {
-            sum += i
-        }
-        
-        completion(.success(sum))
-    }
-}
-
-public class ACSum0To1000000000Even: AwaitConcurrentSupport<Int> {
-    
-    public override func execute(_ completion: @escaping (AwaitCompletableResult<Int>) -> Void) {
-        var sum = 0
-        for i in 0...10000 where i % 2 == 0 {
-            sum += i
-        }
-        
-        completion(.failure(AwaitError.timeout))//.success(sum))
-    }
-    
-    public override var supported: Supported {
-        return .optional
-    }
-}
-
-
-public struct ACResultSum: AwaitCompletable {
-    public var timeout: DispatchTimeInterval?
-    
-    private var odd: Int
-    private var even: Int
-    
-    public init(_ odd: Int, _ even: Int) {
-        self.odd = odd
-        self.even = even
-    }
-    
-    public func execute(_ completion: @escaping (AwaitCompletableResult<String>) -> Void) {
-        let sum = odd + even
-        completion(.success(sum.description))
-    }
-}
+//public class ACSum0To1000000000Odd: AwaitConcurrentSupport<Int> {
+//
+//    public override func execute(_ completion: @escaping (AwaitCompletableResult<Int>) -> Void) {
+//        var sum = 0
+//        for i in 0...1000000000 where i % 2 == 1 {
+//            sum += i
+//        }
+//
+//        completion(.success(sum))
+//    }
+//}
+//
+//public class ACSum0To1000000000Even: AwaitConcurrentSupport<Int> {
+//
+//    public override func execute(_ completion: @escaping (AwaitCompletableResult<Int>) -> Void) {
+//        var sum = 0
+//        for i in 0...10000 where i % 2 == 0 {
+//            sum += i
+//        }
+//
+//        completion(.failure(AwaitError.timeout))//.success(sum))
+//    }
+//
+//    public override var supported: Supported {
+//        return .optional
+//    }
+//}
+//
+//
+//public struct ACResultSum: AwaitCompletable {
+//    public var timeout: DispatchTimeInterval?
+//
+//    private var odd: Int
+//    private var even: Int
+//
+//    public init(_ odd: Int, _ even: Int) {
+//        self.odd = odd
+//        self.even = even
+//    }
+//
+//    public func execute(_ completion: @escaping (AwaitCompletableResult<String>) -> Void) {
+//        let sum = odd + even
+//        completion(.success(sum.description))
+//    }
+//}
 
 
 class ViewController: UIViewController {
@@ -64,34 +64,34 @@ class ViewController: UIViewController {
         // await block is Synchronous
         
         // serial call
-        async {
-            do {
-                let odd = try await(ACSum0To1000000000Odd())
-                let even = try await(ACSum0To1000000000Even())
-                let sum = try await(ACResultSum(odd, even))
-                print(">>> odd: \(odd)")
-                print(">>> even: \(even)")
-                print(">>> sum: \(sum)")
-            }
-            catch {
-                // Type: AwaitError
-                print("[Error] \(error)")
-            }
-        }
-        
-        // concurrent call
-        async {
-            do {
-                let result = try await(ACSum0To1000000000Even(), ACSum0To1000000000Odd())
-                print(">>> count: \(result.count)")
-                print(">>> even: \(result[1])")
-                print(">>> odd: \(result[0])")
-            }
-            catch {
-                // Type: AwaitConcurrentError.concurrent<T>
-                print("[Error] \(error)")
-            }
-        }
+//        async {
+//            do {
+//                let odd = try await(ACSum0To1000000000Odd())
+//                let even = try await(ACSum0To1000000000Even())
+//                let sum = try await(ACResultSum(odd, even))
+//                print(">>> odd: \(odd)")
+//                print(">>> even: \(even)")
+//                print(">>> sum: \(sum)")
+//            }
+//            catch {
+//                // Type: AwaitError
+//                print("[Error] \(error)")
+//            }
+//        }
+//        
+//        // concurrent call
+//        async {
+//            do {
+//                let result = try await(ACSum0To1000000000Even(), ACSum0To1000000000Odd())
+//                print(">>> count: \(result.count)")
+//                print(">>> even: \(result[1])")
+//                print(">>> odd: \(result[0])")
+//            }
+//            catch {
+//                // Type: AwaitConcurrentError.concurrent<T>
+//                print("[Error] \(error)")
+//            }
+//        }
     }
     
     @IBAction private func onTouchUpInside (_ sender: Any) {
